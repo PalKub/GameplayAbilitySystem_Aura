44580e9cea4ea6810db3f669e43a1fce9dcc5bad
Jakub Paluch <paluchk@gmail.com>
2024-07-09 13:14:11 +0200

    Added a gameplay tags class

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright Jakub Paluch
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native GameplayTags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+ static const FAuraGameplayTags& Get() { return GameplayTags; }
+ static void InitializeNativeGameplayTags();
+protected:
+
+private:
+ static FAuraGameplayTags GameplayTags;
+};
