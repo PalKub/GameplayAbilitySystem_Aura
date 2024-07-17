fdc3f8d401a18249ed2fd0d62d1d9cc67c850433
Jakub Paluch <paluchk@gmail.com>
2024-07-17 09:04:45 +0200

    Added a data asset for storing default character attributes

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Jakub Paluch
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
