84d5e6c9e647059af017100ea2bfdf061abebb50
Jakub Paluch <paluchk@gmail.com>
2024-08-01 15:40:51 +0200

    Implemented custom GameplayEffectContext

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Copyright Jakub Paluch
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
