778ca7f5bf1072cdeea036d7b9fe36e78781f881
Jakub Paluch <paluchk@gmail.com>
2024-08-08 14:50:21 +0200

    Impact sounds and effects are now executed by gameplay cues

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -32,7 +32,8 @@ public:
 	virtual bool IsDead_Implementation() const override;
 	virtual AActor* GetAvatar_Implementation() override;
 	virtual TArray<FTaggedMontage> GetAttackMontages_Implementation() override;
-	virtual UNiagaraSystem* GetBloodEffect_Implementation() override; 
+	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
+	virtual FTaggedMontage GetTaggedMontageByTag_Implementation(const FGameplayTag& MontageTag) override;
 	/** end Combat Interface */
 
 	UFUNCTION(NetMulticast, Reliable)
