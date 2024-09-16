9761480f05ad2f81018ac6d2f6aea00936b7b2e9
Jakub Paluch <paluchk@gmail.com>
2024-09-16 10:04:57 +0200

    Loading the world state

--- Source/Aura/Public/Interaction/SaveInterface.h
+++ Source/Aura/Public/Interaction/SaveInterface.h
@@ -6,7 +6,6 @@
 #include "UObject/Interface.h"
 #include "SaveInterface.generated.h"
 
-// This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class USaveInterface : public UInterface
 {
@@ -20,6 +19,11 @@ class AURA_API ISaveInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool ShouldLoadTransform();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void LoadActor();
 };
